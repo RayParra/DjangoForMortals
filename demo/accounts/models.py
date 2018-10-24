f0c627897838fb7761b45845b46f1127a58e3ed0
Ray Parra <rayparra@MacBook-Air-de-Ray.local>
2018-10-24 14:33:47 -0700

    UserProfileManager

--- demo/accounts/models.py
+++ demo/accounts/models.py
@@ -4,12 +4,28 @@ from django.conf import settings
 
 
 
+class UserProfileManager(models.Manager):
+	use_for_related_fields = True
+	def all(self):
+		#print(dir(self))
+		qs = self.get_queryset().all()
+		print(self.instance)
+		if self.instance:
+				qs = qs.exclude(user=self.instance)
+
+		return qs
 
 
 class UserProfile(models.Model):
 	user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
 	following = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='followed_by') 
 
+	objects = UserProfileManager()
 
 	def __str__(self):
 		return str(self.following.all().count())
+
+
+	def get_following(self):
+		users = self.following.all()
+		return users.exclude(username=self.user)
