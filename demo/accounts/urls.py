a38aab38898996122563fcb617ea7d7a78bbcd35
Ray Parra <rayparra@MacBook-Air-de-Ray.local>
2018-10-25 16:37:06 -0700

    follow btn

--- demo/accounts/urls.py
+++ demo/accounts/urls.py
@@ -7,4 +7,5 @@ app_name = "user_profiles"
 
 urlpatterns = [
 	path('user_profile/<str:username>/', views.UserProfileDetail.as_view(), name="user_profile"),
+	path('user_profile/<str:username>/follow', views.UserFollowView.as_view(), name="followView"),	
 ]
--- demo/accounts/views.py
+++ demo/accounts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from django.views import generic
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic, View
 from django.contrib.auth import get_user_model
 # Create your views here.
+
+from .models import UserProfile
 
 
 Users = get_user_model()
@@ -13,3 +15,16 @@ class UserProfileDetail(generic.DetailView):
 
 	def get_object(self):
 		return get_object_or_404(Users, username__iexact=self.kwargs.get("username"))
+
+
+
+class UserFollowView(View):
+	def get(self, request, username, *args, **kwargs):
+		toggle_user = get_object_or_404(Users, username__iexact=username)
+		if request.user.is_authenticated:
+			user_profile, created = UserProfile.objects.get_or_create(user=request.user)
+			if toggle_user in user_profile.following.all():
+				user_profile.following.remove(toggle_user)
+			else:
+				user_profile.following.add(toggle_user)
+		return redirect("user_profiles:user_profile", username=username)
